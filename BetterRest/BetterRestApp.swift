56dbb8179061aeb591e057da0236e0da7d234414
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-18 21:54:45 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mohit Sengar on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
